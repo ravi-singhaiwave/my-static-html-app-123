0be3287e65f770562065eb597db889d1605fa051
ravi-singhaiwave <163492371+ravi-singhaiwave@users.noreply.github.com>
2025-09-06 14:03:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ app.post('/api/prompt', (req, res) => {
     const userPrompt = req.body.prompt;
     const clientId = req.headers['clientid1234'];
     const clientSecret = req.headers['clientsecret1234'];
-     const content-type = req.headers['application/json'];
     console.log(`Received prompt: ${userPrompt}`);
       console.log(`Client ID: ${clientId}`);
     console.log(`Client Secret: ${clientSecret}`);
-    console.log(`Client Secret: ${content-type}`);
 
     const mockResponse = {
         response: `This is a mock response to your prompt: "${userPrompt}".`
